b72f9d0d68dd516b1164b9c7031676614f90dda4
Jason Roy <jason.wesley.roy@gmail.com>
2017-02-17 17:58:56 -0700

    Initial Iteration

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1487378943,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/zombie-sheet0.png",
+		"images/portal-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/fullunicolorsnolines-sheet0.png",
+		"images/fullspecificcolorswithlines-sheet0.png",
+		"images/fullspecificcolorswithlines2-sheet0.png",
+		"images/idle_aim__-sheet0.png",
+		"images/zom-sheet0.png",
+		"images/zom-sheet1.png",
+		"images/zom-sheet2.png",
+		"images/bullet-sheet0.png",
+		"images/explosion-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
